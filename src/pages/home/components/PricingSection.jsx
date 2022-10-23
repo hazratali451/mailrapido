9732fdf2357224c4ccd927829549e5e5accfadef
rafuj <raihanrafuj9978@gmail.com>
2022-10-23 23:07:26 +0600

    Pricing Section Added

--- src/pages/home/components/PricingSection.jsx
+++ src/pages/home/components/PricingSection.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import check from "../../../assets/images/pricing-plan/check.svg";
+import pricing_bg from "../../../assets/images/pricing-plan/pricing-bg.png";
 import SectionHeader from "../../../components/SectionHeader";
+
 const PricingSection = () => {
 	return (
 		<>
 			<section className="pricing-section">
+				<div
+					className="pricing-shape"
+					style={{
+						background: `url(${pricing_bg}) no-repeat top center / cover`,
+					}}
+				></div>
 				<div className="container">
 					<SectionHeader
 						className="pricing-section-header"
@@ -11,6 +21,111 @@ const PricingSection = () => {
 						title="The right price for you, whoever you are"
 						text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit numquam eligendi quos odit doloribus molestiae voluptatum. "
 					/>
+					<div className="__pricing">
+						<div className="__pricing-item">
+							<div className="__pricing-item-top">
+								<h6 className="name">Public</h6>
+								<div className="price">
+									<span className="symbol">$</span>
+									<h3 className="amount">0</h3>
+									<small>/Month</small>
+								</div>
+							</div>
+							<div className="__pricing-item-bottom">
+								<ul>
+									<li>
+										<img src={check} alt="" />
+										<span>Unlimited temporary emails</span>
+									</li>
+									<li>
+										<img src={check} alt="" />
+										<span>
+											Download local copy of received emails
+										</span>
+									</li>
+									<li>
+										<img src={check} alt="" />
+										<span>Keep the same email up to 5 days</span>
+									</li>
+								</ul>
+								<Link to="#" className="__pricing-btn">
+									Start your trial
+								</Link>
+							</div>
+						</div>
+						<div className="__pricing-item">
+							<div className="__pricing-item-top">
+								<h6 className="name">Premium</h6>
+								<div className="price">
+									<span className="symbol">$</span>
+									<h3 className="amount">9</h3>
+									<small>/Month</small>
+								</div>
+							</div>
+							<div className="__pricing-item-bottom">
+								<ul>
+									<li>
+										<img src={check} alt="" />
+										<span>All public offer features included</span>
+									</li>
+									<li>
+										<img src={check} alt="" />
+										<span>Keep the same emails up to 1 year</span>
+									</li>
+									<li>
+										<img src={check} alt="" />
+										<span>
+											Save received emails content up to 1 month
+										</span>
+									</li>
+									<li>
+										<img src={check} alt="" />
+										<span>Forward received emails</span>
+									</li>
+									<li>
+										<img src={check} alt="" />
+										<span>
+											Use multi emails at once (Up to 30 mailbox)
+										</span>
+									</li>
+								</ul>
+								<Link to="#" className="__pricing-btn active">
+									Start your trial
+								</Link>
+							</div>
+						</div>
+						<div className="__pricing-item">
+							<div className="__pricing-item-top">
+								<h6 className="name">Public</h6>
+								<div className="price">
+									<span className="symbol">$</span>
+									<h3 className="amount">0</h3>
+									<small>/Month</small>
+								</div>
+							</div>
+							<div className="__pricing-item-bottom">
+								<ul>
+									<li>
+										<img src={check} alt="" />
+										<span>Unlimited temporary emails</span>
+									</li>
+									<li>
+										<img src={check} alt="" />
+										<span>
+											Download local copy of received emails
+										</span>
+									</li>
+									<li>
+										<img src={check} alt="" />
+										<span>Keep the same email up to 5 days</span>
+									</li>
+								</ul>
+								<Link to="#" className="__pricing-btn">
+									Start your trial
+								</Link>
+							</div>
+						</div>
+					</div>
 				</div>
 			</section>
 		</>
